1a81252de11621e56b2a1a0a2c9437e06ef5a041
anushkax1 <144704315+anushkax1@users.noreply.github.com>
2023-10-14 22:47:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import About from './components/About';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Skills from './components/Skills'; 
-import Experience from './components/Experience';
+import Header from '.Header';
+import About from '.About';
+import Projects from '.Projects';
+import Contact from './Contact';
+import Skills from './Skills'; 
+import Experience from './Experience';
 
 function App() {
   return (
