c587a636880e92095dd36c57ee84ce83da4abbe3
anushkax1 <144704315+anushkax1@users.noreply.github.com>
2023-10-14 22:43:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,23 @@
-import { useState } from 'react';
+import React from 'react';
 import './App.css';
-function App() {
-  const [value, setValue] = useState('');
-  return (
-    <div className="calculator-grid">
-      <div className="output">
-        <input type="text" value={value}/>
-      </div>
+import Header from './components/Header';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Skills from './components/Skills'; 
+import Experience from './components/Experience';
 
-      <input type="button" className="span-two" value="AC" onClick={e=> setValue('')}/>
-<input type="button" value="DEL" onClick={e=> setValue(value.slice(0, -1))}/>
-<input type="button" value="+" onClick={e=> setValue(value + e.target.value)}/>
-<input type="button" value="1" onClick={e=> setValue(value + e.target.value)}/>
-<input type="button" value="2" onClick={e=> setValue(value + e.target.value)}/>
-<input type="button" value="3" onClick={e=> setValue(value + e.target.value)}/>
-<input type="button" value="/" onClick={e=> setValue(value + e.target.value)}/>
-<input type="button" value="4" onClick={e=> setValue(value + e.target.value)}/>
-<input type="button" value="5" onClick={e=> setValue(value + e.target.value)}/>
-<input type="button" value="6" onClick={e=> setValue(value + e.target.value)}/>
-<input type="button" value="+" onClick={e=> setValue(value + e.target.value)}/>
-<input type="button" value="7" onClick={e=> setValue(value + e.target.value)}/>
-<input type="button" value="8" onClick={e=> setValue(value + e.target.value)}/>
-<input type="button" value="9" onClick={e=> setValue(value + e.target.value)}/>
-<input type="button" value="-" onClick={e=> setValue(value + e.target.value)}/>
-<input type="button" value="0" onClick={e=> setValue(value + e.target.value)}/>
-<input type="button" value="." onClick={e=> setValue(value + e.target.value)}/>
-<input type="button" className="span-two" value="=" onClick={e=> setValue(eval(value))}/>
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <About />
+        <Experience />
+        <Projects />
+      <Skills />
+      <Contact />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
